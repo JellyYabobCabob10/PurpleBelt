f0c86e4fdec17ff219fb160800a2fe1dc1330a42
godzilaer <>
2025-04-11 16:45:20 -0400

    aaa

---
+++ JC - DontTouchTheCubes (Also Chopsticks)/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    [Header("Rigidbody")]
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(0, 0, 7 * Time.deltaTime);
+        Time.timeScale += Time.fixedDeltaTime * 0.01f;
+        rb.velocity += transform.rotation * (Vector3.right * Input.GetAxisRaw("Horizontal") * 10f * Time.deltaTime);
+        rb.velocity += transform.rotation * (Vector3.up * Input.GetAxisRaw("Vertical") * 10f * Time.deltaTime);
+        float clampedX = Mathf.Clamp(transform.position.x, -300f, 300f);
+        float clampedZ = Mathf.Clamp(transform.position.z, -300f, 300f);
+        transform.position = new Vector3(clampedX, 0, clampedZ);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ JC - SuperShapes/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Default Movement Speed")]
+    public float moveSpeed = 200;
+    float movement = 0;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
+    void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+    }
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+
+        Time.timeScale = 0;
+    }
+}
